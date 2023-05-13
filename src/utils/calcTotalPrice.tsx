913fb9d5f72670700f597f9165102ec2d41595b7
Maksim Vladimirov <vl4dimirov@gmail.com>
2023-05-13 11:49:13 +0300

    fix deploy

--- src/utils/calcTotalPrice.tsx
+++ src/utils/calcTotalPrice.tsx
@@ -1,4 +1,4 @@
-import { CartItem } from '../redux/slices/cartSlice';
+import { CartItem } from '../redux/cart/types';
 
 export const calcTotalPrice = (items: CartItem[]) => {
   return items.reduce((acc, obj) => (acc += obj.price * obj.count), 0);
